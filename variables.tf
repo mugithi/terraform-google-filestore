ba79ccdd63ada9d09e317404af15c26ad80a7088
mugithi <isackaranja@gmail.com>
2020-02-18 14:31:11 -0800

    added variables

--- variables.tf
+++ variables.tf
@@ -14,47 +14,49 @@
  * limitations under the License.
  */
 
-variable "project_id" {
-  description = "project - (Optional) The ID of the project in which the resource belongs. If it is not provided, the provider project is used."
+
+
+ variable "filestore_name" {
+  description = "name - (Required) The resource name of the instance."
 }
 
 variable "zone" {
   description = "zone - (Required) The name of the Filestore zone of the instance."
-}
-
-variable "filestore_name" {
-  description = "name - (Required) The resource name of the instance."
+  default = "us-central1-a"
 }
 
 variable "tier" {
-  description = "tier - (Required) The service tier of the instance."  
-}
-
-variable "file_shares_name" {
-  description = "file_shares_name - (Required) The name of the fileshare (16 characters or less)"
+  description = "tier - (Required) The service tier of the instance. Can be PREMIUM or STANDARD"
+  default = "PREMIUM"
 }
 
 variable "capacity_gb" {
   description = "capacity_gb - (Required) File share capacity in GiB. This must be at least 1024 GiB for the standard tier, or 2560 GiB for the premium tier."
 }
 
+variable "file_shares_name" {
+  description = "file_shares_name - (Required) The name of the fileshare (16 characters or less)"
+}
+
 variable "network" {
   description = "network - (Required) The name of the GCP VPC network to which the instance is connected."
+  default = ["MODE_IPV4"]
 }
 
 variable "modes" {
   description = "modes - (Required) IP versions for which the instance has IP addresses assigned."
 }
 
-variable "reserved_ip_range" {
-  description = "reserved_ip_range - (Optional) A /29 CIDR block that identifies the range of IP addresses reserved for this instance."
-}
 
-variable "ip_addresses" {
-  description = "ip_addresses - A list of IPv4 or IPv6 addresses."
-}
+# variable "reserved_ip_range" {
+#   description = "reserved_ip_range - (Optional) A /29 CIDR block that identifies the range of IP addresses reserved for this instance."
+# }
 
-variable "labels" {
-  description = "labels - (Optional) Resource labels to represent user-provided metadata."
-}
+# variable "ip_addresses" {
+#   description = "ip_addresses - A list of IPv4 or IPv6 addresses."
+# }
+
+# variable "labels" {
+#   description = "labels - (Optional) Resource labels to represent user-provided metadata."
+# }
 
